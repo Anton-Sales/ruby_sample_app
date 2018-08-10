05ae7d1b8cc49887ae588b6f5a58c9f14735646b
anton-sales <anton.arus@gmail.com>
2018-08-10 17:00:58 +1200

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get root" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url # test expects home to be a webpage, accessed using GET req. Make sure to we receive :success is saying (status code 200, OK)
     assert_response :success
